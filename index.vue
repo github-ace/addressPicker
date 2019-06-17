534ff9b2e4e521211cc305c3b0c1ba63a3e16626
github-ace <2433195678@qq.com>
2019-06-17 10:17:03 +0800

    Create index.vue

---
+++ index.vue
@@ -0,0 +1,34 @@
+<template>
+  <input type="text" @click="choose" :value="value">
+</template>
+<script>
+import "weui";
+import weui from "weui.js";
+import address from "./address";
+console.log(address);
+export default {
+  props: ["value"],
+  data() {
+    return {
+      address: address
+    };
+  },
+  methods: {
+    choose() {
+      weui.picker(address, {
+        defaultValue: [0, 0, 0],
+        onChange: result => {
+          // console.log(result);
+        },
+        onConfirm: result => {
+          this.$emit("choose", result);
+        },
+        id: "multiPickerBtn"
+      });
+    }
+  },
+  mounted() {}
+};
+</script>
+<style lang="less" scoped>
+</style>
